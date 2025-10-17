3a1d6b14248f0fd8f855a7e3052a8c62b7304ff8
Student-Prabin <dahalprabin74@gmail.com>
2025-10-17 13:51:37 +0545

    fingers crossed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -10,7 +10,7 @@ const syncUserCreation = inngest.createFunction(
   { event: 'clerk/user.created' },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
-    let username = email_addresses[0].email_addresses.split('@')[0]
+    let username = email_addresses[0].email_address.split('@')[0]
     // Check availability of username
     const user = await User.findOne({ username })
     if (user) {
@@ -18,7 +18,7 @@ const syncUserCreation = inngest.createFunction(
     }
     const userData = {
       _id: id,
-      email: email_addresses[0].email_addresses,
+      email: email_addresses[0].email_address,
       full_name: first_name + " " + last_name,
       profile_picture: image_url,
       username
@@ -39,7 +39,7 @@ const syncUserUpdation = inngest.createFunction(
       full_name: first_name + " " + last_name,
       profile_picture: image_url,
     }
-    await User.findById(id, updatedUserData);
+    await User.findByIdAndUpdate(id, updatedUserData);
   }
 )
 
